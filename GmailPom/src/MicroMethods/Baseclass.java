4c899b52b64df7e3a69edce3127ef543c292da7d
Rushikesh <dixit.rushikesh@gmail.com>
2017-06-16 19:24:25 +0530

    First Commit

---
+++ GmailPom/src/MicroMethods/Baseclass.java
@@ -0,0 +1,13 @@
+package MicroMethods;
+
+import org.openqa.selenium.WebDriver;
+
+public class Baseclass {
+	public static WebDriver driver;
+	public static boolean bResult;
+
+	public Baseclass(WebDriver driver) {
+		Baseclass.driver = driver;
+		Baseclass.bResult = true;
+	}
+}
---
+++ GmailPom/src/dataTable/triangle.java
@@ -0,0 +1,32 @@
+package dataTable;
+
+public class triangle 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	
+}
+
+/*
+A
+BB
+CCC
+DDDD*/
+
+/*int alphabet=65;
+for(int i=1;i<5;i++)
+	 
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print((char)alphabet);
+}
+System.out.println();
+alphabet++;*/
+
